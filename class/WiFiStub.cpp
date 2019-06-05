17512ca29f948baaecd5f1ca21dd85317d6b11fd
matshec <mmasakra@gmail.com>
2019-06-06 00:56:23 +0200

    add comments

--- class/WiFiStub.cpp
+++ class/WiFiStub.cpp
@@ -18,15 +18,18 @@ class WiFi {
     public:
 
     void setReturnValue(void* data){
+        //use  this to set return value for next called method
         returnData = data;
     }
 
     int getCallCount(){
+        //check how many time class has been called
         return callCount;
     }
 
     
     void resetCallCount(){
+        //reset call count
         callCount = 0;
     }
     int scanNetworks(){
