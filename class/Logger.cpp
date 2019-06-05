faf0f82a52e6f018eb1ef4b1cec148690b7bdc77
matshec <mmasakra@gmail.com>
2019-06-05 23:35:35 +0200

    fix logger

--- class/Logger.cpp
+++ class/Logger.cpp
@@ -1,11 +1,11 @@
 #include <string>
-
+using namespace std;
 class Logger {
 public:
     string createLog (string, string, bool);
 };
 
-string Logger::createLog(string name, string strength, bool secured) {} {
+string Logger::createLog(string name, string strength, bool secured)  {
     string log = "Network name: " + name + "\nSignal strength: " + strength;
     if (secured) {
         log += "\nNot secured with password";
